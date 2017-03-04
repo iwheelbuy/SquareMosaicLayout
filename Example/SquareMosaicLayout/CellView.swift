cfddcdb381af22862bf34066df4e2bc54c819913
Mikhail Vasilev <iwheelbuy@MacBook-Pro-Mikhail.local>
2017-03-04 16:09:31 +0700

    Remove autoresizing mask

--- Example/SquareMosaicLayout/CellView.swift
+++ Example/SquareMosaicLayout/CellView.swift
@@ -6,22 +6,22 @@ final class CellView: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(imageView)
-        layer.borderWidth = 0.5
-        layer.borderColor = UIColor.groupTableViewBackground.cgColor
-        self.contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        self.imageView.frame = bounds
-        self.imageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.contentView.addSubview(imageView)
+        self.layer.borderWidth = 0.5
+        self.layer.borderColor = UIColor.groupTableViewBackground.cgColor
         self.imageView.contentMode = .scaleAspectFill
         self.contentView.clipsToBounds = true
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var bounds: CGRect {
+        didSet {
+            contentView.frame = bounds
+            imageView.frame = bounds
+        }
     }
 }
 
